01f39fd54226c6b5297cf559864a5aefc013e0ce
Karthik B K <karthik.bk@incoresemi.com>
2025-02-26 08:34:44 +0530

    minor cleanup

--- lox/Token.java
+++ lox/Token.java
@@ -1,4 +1,4 @@
-package com.craftinginterpreters.lox;
+package com.karpreters.lox;
 
 class Token {
   final TokenType type;
--- lox/TokenTypes.java
+++ lox/TokenTypes.java
@@ -1,4 +1,4 @@
-package com.craftinginterpreters.lox;
+package com.karpreters.lox;
 
 enum TokenType {
   // Single-character tokens.
