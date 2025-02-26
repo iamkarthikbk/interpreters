4db0a8af62da7591f6031cdc75b08719b30350f5
Karthik B K <karthik.bk@incoresemi.com>
2025-02-26 08:32:55 +0530

    lox: tokens and token types
    bascially defined what tokens our scanner will be able to scan, and a class for a Token

---
+++ lox/Token.java
@@ -0,0 +1,20 @@
+package com.craftinginterpreters.lox;
+
+class Token {
+  final TokenType type;
+  final String lexeme;
+  final Object literal;
+  final int line;
+
+  Token(TokenType type, String lexeme, Object literal, int line) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
+
+  // this is used for error reporting. a line number and column number will be more useful.
+  public String toString() {
+    return type + " " + lexeme + " " + literal;
+  }
+}
